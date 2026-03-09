6212cf3425548069ba123652b84180b5514756be
Shredhaya <shredhayagoel@gmail.com>
2026-03-10 02:14:27 +0530

    Work updates and Navbar updates

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,18 @@
 import React, { useState } from "react";
 import assets from "../assets/assets";
 import ThemeToggleBtn from "./ThemeToggleBtn";
+import { motion } from "framer-motion";
 
 const Navbar = ({ theme, settheme }) => {
   const [sidebaropen, setsidebaropen] = useState(false);
 
   return (
-    <div className="flex justify-between items-center px-4 sm:px-12 lg:px-24 xl:px-40 py-4 sticky top-0 z-20 backdrop-blur-xl   font-medium bg-white/30 dark:bg-gray-900/70">
+    <motion.div
+      initial={{ opacity: 0, y: -50 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.6, ease: "easeInOut" }}
+      className="flex justify-between items-center px-4 sm:px-12 lg:px-24 xl:px-40 py-4 sticky top-0 z-20 backdrop-blur-xl   font-medium bg-white/30 dark:bg-gray-900/70"
+    >
       <img
         src={theme === "dark" ? assets.logo_dark : assets.logo}
         alt="logo"
@@ -62,13 +68,13 @@ const Navbar = ({ theme, settheme }) => {
           className="w-8 sm:hidden"
         ></img>
         <a
-          href="#contact-us"
+          href="#contact"
           className="text-sm max-sm:hidden flex items-center gap-2 bg-blue-600  text-white px-6 py-2 rounded-full cursor-pointer hover:scale-103 transition-all"
         >
           Connect <img src={assets.arrow_icon} width={14} alt="" />
         </a>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
--- src/components/OurWork.jsx
+++ src/components/OurWork.jsx
@@ -25,7 +25,7 @@ const OurWork = () => {
 
   return (
     <div
-      id="our work"
+      id="work"
       className=" flex flex-col items-center gap-7 px-4 sm:px-12 lg:px-24 xl:px-40 pt-30 text-gray-700 dark:text-white"
     >
       <Tittle
