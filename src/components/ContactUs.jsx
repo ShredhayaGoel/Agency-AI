0ba8ba7d6daadc7dcda14acda9393908b364314b
Shredhaya <shredhayagoel@gmail.com>
2026-03-10 02:16:29 +0530

    Added Contact Information

---
+++ src/components/ContactUs.jsx
@@ -0,0 +1,103 @@
+import React from "react";
+import Tittle from "./Tittle";
+import assets from "../assets/assets";
+import toast from "react-hot-toast";
+
+const ContactUs = () => {
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "1c4e4730-c5bd-463a-ae20-9e41312deabe");
+
+    try {
+      const response = await fetch("https://api.web3forms.com/submit", {
+        method: "POST",
+        body: formData,
+      });
+
+      const data = await response.json();
+
+      if (data.success) {
+        toast.success("Message sent successfully!");
+        event.target.reset();
+      } else {
+        console.log("Error", data);
+        toast.error("Failed to send message. Please try again.");
+      }
+    } catch (error) {
+      console.log("Error", error);
+      toast.error(
+        error.message || "An unexpected error occurred. Please try again.",
+      );
+    }
+  };
+  return (
+    <div
+      id="contact"
+      className="flex flex-col items-center gap-7 px-4 sm:px-12 lg:px-24 xl:px-40 pt-30 text-gray-800 dark:text-white"
+    >
+      <Tittle
+        tittle="Reach out to us"
+        desc="Have questions or want to get in touch? We'd love to hear from you."
+      />
+
+      <form
+        onSubmit={onSubmit}
+        className="grid sm:grid-cols-2 gap-3 sm:gap-5 max-w-2xl w-full"
+      >
+        <div>
+          <p className="mb-2 text-sm font-medium"> Your Name </p>
+          <div className="flex pl-3 rounded-lg border border-gray-300 dark:border-gray-600">
+            <img src={assets.person_icon} alt="" />
+            <input
+              name="name"
+              type="text"
+              placeholder="Enter your name "
+              className="w-full p-3 text-sm outline-none"
+              required
+            />
+          </div>
+        </div>
+
+        <div>
+          <p className="mb-2 text-sm font-medium"> Email-id </p>
+          <div className="flex pl-3 rounded-lg border border-gray-300 dark:border-gray-600">
+            <img src={assets.email_icon} alt="" />
+            <input
+              name="email"
+              type="text"
+              placeholder="Enter your email-id "
+              className="w-full p-3 text-sm outline-none"
+              required
+            />
+          </div>
+        </div>
+
+        <div className="sm:col-span-2">
+          <p className="mb-2 text-sm font-medium"> Message </p>
+          <textarea
+            name="message"
+            rows={8}
+            placeholder="Enter your Message"
+            className="w-full
+                      p-3 text-sm outline-none rounded-lg border border-gray-300 dark:border-gray-600"
+            required
+          />
+        </div>
+        <button
+          type="submit"
+          className=" w-max flex gap-2 bg-blue-500 text-white
+         text-sm
+         px-10 py-3 rounded-full cursor-pointer hover: scale-103 transition-all"
+        >
+          Submit
+          <img src={assets.arrow_icon} alt="" className="w-4" />
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default ContactUs;
+// 1c4e4730-c5bd-463a-ae20-9e41312deabe
